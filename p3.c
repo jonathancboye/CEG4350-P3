b88e3a397d2e53e7eccddd99479c0cc38617153b
Jonathan <carpenter.prime@gmail.com>
2015-07-24 09:18:19 -0400

    implemented irmovl

--- p3.c
+++ p3.c
@@ -5,26 +5,51 @@
  *      Author: jon
  */
 
-/* NEXT TASK
- * Figure out proper length for program counter
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 
 #define MEMORYSIZE 5000
+#define DEBUG 1 //turn debugging on or off
 
-typedef unsigned char mtype;
+typedef char mtype;
 
 //contains the condition codes
-typedef struct ConditionCodes {
+typedef struct {
 	uint8_t zf:1;
 	uint8_t sf:1;
 	uint8_t of:1;
 } ConditionCodes;
 
-/*Registers
+typedef struct {
+	uint8_t bit_1:1;
+	uint8_t bit_2:1;
+	uint8_t bit_3:1;
+	uint8_t bit_4:1;
+	uint8_t bit_5:1;
+	uint8_t bit_6:1;
+	uint8_t bit_7:1;
+	uint8_t bit_8:1;
+}Bits;
+
+typedef struct {
+	uint8_t lower:4;
+	uint8_t upper:4;
+}Nibbles;
+
+typedef struct {
+	uint32_t byte_1:8; //most significant byte
+	uint32_t byte_2:8;
+	uint32_t byte_3:8;
+	uint32_t byte_4:8; //least significant byte
+}Bytes;
+
+typedef struct {
+	uint32_t lower:16;
+	uint32_t upper:16;
+}Words;
+
+/*Register
  * integer: full register, 32bits
  * upper: upper half of the register, 16bits
  * lower: lower half of the register, 16bits
@@ -33,19 +58,22 @@ typedef struct ConditionCodes {
  * byte_3: third byte, 8bits
  * byte_4: fourth byte, 8bits
  */
-typedef union registers {
-	uint32_t integer:32;
-	struct sections {
-		uint16_t upper:16;
-		uint16_t lower:16;
-	} section;
-	struct bytes {
-		uint8_t byte_1:8; //most significant byte
-		uint8_t byte_2:8;
-		uint8_t byte_3:8;
-		uint8_t byte_4:8; //least significant byte
-	} byte;
-} Register;
+
+typedef union {
+	uint32_t reg;
+	Words words;
+	Bytes bytes;
+}Register;
+
+typedef union {
+	uint8_t byte;
+	Nibbles nibbles;
+	Bits bits;
+}Byte;
+
+
+void irmovl(int PC, mtype *memory, Register *regs); //immediate -> register
+
 
 int main(int argc, char *argv[]) {
 	/* 32bit program registers */
@@ -57,28 +85,37 @@ int main(int argc, char *argv[]) {
 	Register edi;
 	Register esp; //stack pointer register
 	Register ebp;
-	int PC; //program counter, points to next instruction in memory
+	Register regs[8] = {eax, ecx, edx, ebx, esi, edi, esp, ebp};
+	int i, PC; //program counter, points to next instruction in memory
+	int instructionLength; //number of bytes that make up an instruction
 	ConditionCodes cc;
 	mtype *memory = calloc(MEMORYSIZE, sizeof(mtype)); //Main memory for y86
 
 	/* setup input */
+
+	//nop
 	memory[0] = 0x10;
-	memory[1] = 0x00;
+
+	//irmovl register 0 <--- 0x0403
+	memory[1] = 0x30;
+	memory[2] = 0x83;
+	//value
+	memory[3] = 0x03;
+	memory[4] = 0x04;
+	memory[5] = 0x01;
+	memory[6] = 0x20;
+
+	//halt
+	memory[7] = 0x00;
 
 	/* initialize program counter and flags and registers*/
 	PC = 0;
 	cc.zf = 0;
 	cc.sf = 0;
 	cc.of = 0;
-	eax.integer = 0;
-	ecx.integer = 0;
-	edx.integer = 0;
-	esi.integer = 0;
-	edi.integer = 0;
-	esp.integer = 0;
-	esp.integer = 0;
-	ebp.integer = 0;
-
+	for(i=0;i < 8; ++i) {
+		regs[i].reg = 0;
+	}
 
 	/* fetch, decode, execute - loop*/
 	while(1) {
@@ -88,24 +125,79 @@ int main(int argc, char *argv[]) {
 		switch(memory[PC]) {
 
 		case 0x00: 				//halt instruction
-			printf("HALT\n");
+			if(DEBUG) {
+				printf("HALT\n");
+			}
 			return EXIT_SUCCESS;
 
 		case 0x10: 				//nop instruction
-			printf("NOP\n");
+			instructionLength = 1;
+			if(DEBUG) {
+				printf("NOP\n");
+			}
+			break;
+
+		case 0x20:
+			if(DEBUG) {
+				printf("rrmovl");	//rrmovl - Register -> Register
+			}
+			break;
+
+		case 0x30:
+			/*
+			 * Byte offsets
+			 * 	1: [0x8, register to store value]
+			 * 	2 - 5: [value]
+			 */
+			instructionLength = 6;
+			if(DEBUG) {
+				printf("irmovl: 0x30"); 	//irmovl - immediate -> Register
+			}
+			irmovl(PC, memory, regs);
 			break;
 
 		default: 				//error
 			printf("Something didn't work");
+			/* no break */
 		}
+
 
 		/* Execute */
 
 		/* Update PC */
-		PC++;
+		PC += instructionLength;
 	}
 
 
 
 	return EXIT_SUCCESS;
+}
+
+void irmovl(int PC, mtype *memory, Register *regs) {
+	Byte b; //next byte
+	int id; //register id
+
+	//get second byte : [upper nibble has register id, lower nibble should have 8 for instruction]
+	b.byte = memory[PC + 1];
+
+	//verify last nibble of instruction
+	if(b.nibbles.upper == 8) {
+
+		id = b.nibbles.lower;
+
+		//move next 4 bytes into register
+		//ugly: *((uint32_t*)&memory[PC + 2])
+		regs[id].bytes.byte_1 = memory[PC + 2];
+		regs[id].bytes.byte_2 = memory[PC + 3];
+		regs[id].bytes.byte_3 = memory[PC + 4];
+		regs[id].bytes.byte_4 = memory[PC + 5];
+
+		if(DEBUG) {
+			printf(" 0x%d%d 0x%2x\n", b.nibbles.upper, id, regs[id].reg);
+		}
+
+	} else {
+		printf("Something is wrong!!");
+		exit(1);
+	}
 }
